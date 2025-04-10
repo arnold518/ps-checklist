9b8e1f63e128116dff208c0bc7b1fef8f7f67e46
arnold518 <arnoldpark03@gmail.com>
2025-04-11 02:30:25 +0800

    update

--- tools/update.py
+++ tools/update.py
@@ -3,16 +3,16 @@ from getBOJ import BOJCrawler
 from getCF import CFCrawler
 from getQOJ import QOJCrawler
 
+CATEGORY = ["ICPC", "Regionals", "Asia Pacific", "Korea"]
 YEAR = "2020"
 FILE_PATH = "../problemlists/icpc/regionals/asia-pacific/korea/2020/"
-CATEGORY = ["ICPC", "Regionals", "Asia Pacific", "Korea"]
 
 BOJ_URL = "https://www.acmicpc.net/category/detail/2339"
 CF_URL = "https://codeforces.com/gym/102920"
 QOJ_URL = "https://qoj.ac/contest/450"
 
 QOJ_USERNAME = "arnold518"
-QOJ_PASSWORD = "password"
+QOJ_PASSWORD = "jaemin518"
 
 # =========================================================================
 
@@ -33,7 +33,7 @@ def update_by_boj(handler, name, url, problems):
         handler.update_problem_value(idx, ["title"], problem["title"], overwrite=True)
         handler.update_problem_value(idx, ["link", "BOJ"], problem["link"])
 
-def boj(bojCrawler):
+def boj(handler, bojCrawler):
     name, problems = bojCrawler.crawl_boj_category(BOJ_URL)
     bojCrawler.parse_boj_pdf_links(FILE_PATH)
     update_by_boj(handler, name, BOJ_URL, problems)
@@ -49,7 +49,7 @@ def update_by_cf(handler, name, url, problems):
         handler.update_problem_value(idx, ["title"], problem["name"], overwrite=False)
         handler.update_problem_value(idx, ["link", "CF"], problem["link"])
 
-def cf(cfCrawler):
+def cf(handler, cfCrawler):
     name, problems = cfCrawler.crawl_cf_contest(CF_URL)
     cfCrawler.parse_cf_pdf_links(FILE_PATH)
     update_by_cf(handler, name, CF_URL, problems)
@@ -66,30 +66,30 @@ def update_by_qoj(handler, name, url, category, problems):
         handler.update_problem_value(idx, ["title"], problem["title"], overwrite=False)
         handler.update_problem_value(idx, ["link", "QOJ"], problem["url"])
 
-def qoj(qojCrawler):
+def qoj(handler, qojCrawler):
     name, category, problems = qojCrawler.crawl_qoj_contest(QOJ_URL)
     qojCrawler.parse_qoj_pdf_links(FILE_PATH)
     update_by_qoj(handler, name, QOJ_URL, category, problems)
 
 # =========================================================================
 
-handler = JSONHandler(FILE_PATH + "problemlist.json")
 bojCrawler = BOJCrawler()
 cfCrawler = CFCrawler()
 qojCralwer = QOJCrawler(QOJ_USERNAME, QOJ_PASSWORD)
 
 # =========================================================================
 
+handler = JSONHandler(FILE_PATH + "problemlist.json")
 handler.clear()
 
 update_by_default(CATEGORY, YEAR)
 
 print_sep()
-boj(bojCrawler)
+boj(handler, bojCrawler)
 print_sep()
-cf(cfCrawler)
+cf(handler, cfCrawler)
 print_sep()
-qoj(qojCralwer)
+qoj(handler, qojCralwer)
 print_sep()
 
 handler.save()
